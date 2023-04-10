e908fdad8668ac2c2dd323a1f0b6ac428123980f
Anna Hrulckova <annahrulkova@yandex.ru>
2023-04-10 13:47:56 +0400

    изменения в pocketJson

--- webpack-config.js
+++ webpack-config.js
@@ -1,10 +1,16 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'development',  
   entry: './src/index.js',
+  module: {
+    rules: [
+       { test: /\.____$/, use: '________' }]
+},
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  plugins: [new________()]
 };
+
